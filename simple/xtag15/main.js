ea0751f2e31d10a962f765f00fa583dea7a9c297
Florian Hehlen <florian.hehlen@bluewin.ch>
2018-10-10 07:55:15 +0200

    factored template into Game() object

--- simple/xtag15/main.js
+++ simple/xtag15/main.js
@@ -24,162 +24,10 @@
      * bootstrap component using x-tag
      */
     xtag.register('rps-xtag', {
-        shadow:` <div class="game">
-                <style>
-                div,section {
-                    font-family: sans-serif;
-                }
-                section {
-                    border: gray solid 1px;
-                    border-radius: 15px;
-                }
-                .game {
-                    border: black solid 2px;
-                    margin: 2px;
-                    border-radius: 15px;
-                    position: relative;
-                    padding: 10px;
-                  }
-                .player-1, .player-2 {
-                    float:left;
-                    width:48%;
-                    box-sizing: border-box;
-                    padding: 5px;
-                    margin: 5px;
-                    height: 250px;
-                }
-                .score {
-                    clear: both;
-                    width: 96%;                                            
-                    background: lightgray;
-                    padding: 5px;
-                    margin: 5px; 
-                    text-align: center;
-                   
-                }
-                .wins {
-                   color: green;
-                }
-                .losses {
-                    color: red;
-                }
-                .results {
-                    font-size: larger;
-                    font-weight: bold;
-                    
-                }
-              </style>
-                            <div>
-                  <section class="player-1">
-                    <h2 class="name">name</h2>
-                    <div class="options">
-                        <h3>Options</h3>
-                        <button class="rock">Rock</button>
-                        <button class="paper">Paper</button>
-                        <button class="scissor">Scissor</button>
-                    </div>
-                    <div class="selection">
-                        <h3>Selection</h3>
-                       <span class="selected-label">nothing selected</span>
-                    </div>
-                  </section>
-                  <section class="player-2">
-                    <h2 class="name">no name</h2>
-                    <div class="options">
-                        <h3>Options</h3>
-                        <span>Computer will reveal choice after you select</span>
-                    </div>
-                    <div class="selection">
-                       <h3>Selection</h3>
-                       <span class="selected-label">nothing selected</span>
-                    </div>
-                  </section>
-              </div>
-                            <section class="score">
-                 <h2>Score</h2>
-                 <div class="results">
-                    <span class="wins">0</span> wins, <span class="ties">0</span> ties, <span class="losses">0</span> losses out of <span class="out-of">0</span>
-                </div>
-              </section>
-</div>`,
-        content:` <div class="game">
-                <style>
-                div,section {
-                    font-family: sans-serif;
-                }
-                section {
-                    border: gray solid 1px;
-                    border-radius: 15px;
-                }
-                .game {
-                    border: black solid 2px;
-                    margin: 2px;
-                    border-radius: 15px;
-                    position: relative;
-                    padding: 10px;
-                  }
-                .player-1, .player-2 {
-                    float:left;
-                    width:48%;
-                    box-sizing: border-box;
-                    padding: 5px;
-                    margin: 5px;
-                    height: 250px;
-                }
-                .score {
-                    clear: both;
-                    width: 96%;                                            
-                    background: lightgray;
-                    padding: 5px;
-                    margin: 5px; 
-                    text-align: center;
-                   
-                }
-                .wins {
-                   color: green;
-                }
-                .losses {
-                    color: red;
-                }
-                .results {
-                    font-size: larger;
-                    font-weight: bold;
-                    
-                }
-              </style>
-                            <div>
-                  <section class="player-1">
-                    <h2 class="name">name</h2>
-                    <div class="options">
-                        <h3>Options</h3>
-                        <button class="rock">Rock</button>
-                        <button class="paper">Paper</button>
-                        <button class="scissor">Scissor</button>
-                    </div>
-                    <div class="selection">
-                        <h3>Selection</h3>
-                       <span class="selected-label">nothing selected</span>
-                    </div>
-                  </section>
-                  <section class="player-2">
-                    <h2 class="name">no name</h2>
-                    <div class="options">
-                        <h3>Options</h3>
-                        <span>Computer will reveal choice after you select</span>
-                    </div>
-                    <div class="selection">
-                       <h3>Selection</h3>
-                       <span class="selected-label">nothing selected</span>
-                    </div>
-                  </section>
-              </div>
-                            <section class="score">
-                 <h2>Score</h2>
-                 <div class="results">
-                    <span class="wins">0</span> wins, <span class="ties">0</span> ties, <span class="losses">0</span> losses out of <span class="out-of">0</span>
-                </div>
-              </section>
-</div>`,
+        /* when used with shadow dom */
+        shadow: game.template,
+        /* when used without shadow dom */
+        content: game.template,
         lifecycle:{
             created:function () {
                 console.log('rps-xtag', 'lifecycle', 'created');
@@ -218,7 +66,85 @@
         return {
             createGame:createGame,
             updateName:updateName,
-            wireUp:wireUp
+            wireUp:wireUp,
+            template:` <div class="game">
+                <style>
+                div,section {
+                    font-family: sans-serif;
+                }
+                section {
+                    border: gray solid 1px;
+                    border-radius: 15px;
+                }
+                .game {
+                    border: black solid 2px;
+                    margin: 2px;
+                    border-radius: 15px;
+                    position: relative;
+                    padding: 10px;
+                  }
+                .player-1, .player-2 {
+                    float:left;
+                    width:48%;
+                    box-sizing: border-box;
+                    padding: 5px;
+                    margin: 5px;
+                    height: 250px;
+                }
+                .score {
+                    clear: both;
+                    width: 96%;                                            
+                    background: lightgray;
+                    padding: 5px;
+                    margin: 5px; 
+                    text-align: center;
+                   
+                }
+                .wins {
+                   color: green;
+                }
+                .losses {
+                    color: red;
+                }
+                .results {
+                    font-size: larger;
+                    font-weight: bold;
+                    
+                }
+              </style>
+                            <div>
+                  <section class="player-1">
+                    <h2 class="name">name</h2>
+                    <div class="options">
+                        <h3>Options</h3>
+                        <button class="rock">Rock</button>
+                        <button class="paper">Paper</button>
+                        <button class="scissor">Scissor</button>
+                    </div>
+                    <div class="selection">
+                        <h3>Selection</h3>
+                       <span class="selected-label">nothing selected</span>
+                    </div>
+                  </section>
+                  <section class="player-2">
+                    <h2 class="name">no name</h2>
+                    <div class="options">
+                        <h3>Options</h3>
+                        <span>Computer will reveal choice after you select</span>
+                    </div>
+                    <div class="selection">
+                       <h3>Selection</h3>
+                       <span class="selected-label">nothing selected</span>
+                    </div>
+                  </section>
+              </div>
+                            <section class="score">
+                 <h2>Score</h2>
+                 <div class="results">
+                    <span class="wins">0</span> wins, <span class="ties">0</span> ties, <span class="losses">0</span> losses out of <span class="out-of">0</span>
+                </div>
+              </section>
+</div>`
         };
 
         function createGame() {
